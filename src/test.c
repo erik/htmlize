0a33175c8647bf536e61b8e21ddc491268ee7990
boredomist <erik.price16@gmail.com>
2011-05-18 16:58:04 -0400

    Fixed possible bug

--- src/test.c
+++ src/test.c
@@ -4,30 +4,30 @@
 
 int main(int argc, char** argv) {
   html_auto_free = 1;
-  string_t* html = htmlize(DOCTYPE_XHTML,
-                           HEAD(
-                                //<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
-                                html_tag("meta",
-                                         ATTRIBUTES("http-equiv=\"Content-type\"",
-                                                    "content=\"text/html" \
-                                                    ";charset=UTF-8\""),
-                                         CONTENT(NULL),
-                                         1),
-
-                                html_tag("meta",
-                                         ATTRIBUTES("name=\"keywords\"",
-                                                    "content=\"notspam\""),
-                                         CONTENT(NULL),
-                                         1),
-                                html_tag("title",
-                                         ATTRIBUTES(NULL),
-                                         CONTENT("TITLE"),
-                                         0)),
-                           BODY(
-                                html_tag("p",
-                                         ATTRIBUTES(NULL),
-                                         CONTENT("PARAGRAPH"),
-                                         0)));
+  string_t* html = 
+    htmlize(DOCTYPE_XHTML,
+            HEAD(
+                 html_tag("meta",
+                          ATTRIBUTES("http-equiv=\"Content-type\"",
+                                     "content=\"text/html"              \
+                                     ";charset=UTF-8\""),
+                          CONTENT(NULL),
+                          1),
+                 
+                 html_tag("meta",
+                          ATTRIBUTES("name=\"keywords\"",
+                                     "content=\"notspam\""),
+                          CONTENT(NULL),
+                          1),
+                 html_tag("title",
+                          ATTRIBUTES(NULL),
+                          CONTENT("TITLE"),
+                          0)),
+            BODY(
+                 html_tag("p",
+                          ATTRIBUTES(NULL),
+                          CONTENT("PARAGRAPH"),
+                          0)));
   
   printf("%s\n", html->str);
 
